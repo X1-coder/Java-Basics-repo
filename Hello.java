d8becf1d16fe54279dbd1550cb81a5c2d107641c
unknown <Bonsu Peter Kofi>
2026-01-19 23:55:39 +0000

    created a variable

--- Hello.java
+++ Hello.java
@@ -3,5 +3,12 @@ public class Hello {
         // This is a simple Java program that prints "Hello, World!" to the console.
         System.out.println("Hello, World!");
 
+        //create a variable to store your name and print it
+        //Define variable type and its name then assign it a value
+        String name = "Nana Bonsu";
+
+        //Concatinate the variable with a string and print it
+        System.out.println("My name is " + name);
+
     }
 }
